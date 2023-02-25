bf0eb364b8108ade03c90382c2d0882e9000cefb
Jorranes <jorranes.oliveira.a@gmail.com>
2023-02-25 12:28:21 -0300

    escrito o conteúdo da entidade, repository, service, controller e o arquivo .properties

--- src/main/java/com/desafio/banco/repository/UsuarioRepository.java
+++ src/main/java/com/desafio/banco/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package com.desafio.banco.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.desafio.banco.entities.UsuarioModel;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer> {
 
 }
